106cda475ff449eca47d86b325412b78d45a22dd
Colin Detwiller <colindetwillerjr@gmail.com>
2022-11-02 12:10:54 -0400

    creating server controller

--- capstone/TenmoServer/Controllers/TransferController.cs
+++ capstone/TenmoServer/Controllers/TransferController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TenmoServer.Models;
+using TenmoServer.DAO;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,7 +14,7 @@ namespace TenmoServer.Controllers
     [ApiController]
     public class TransferController : ControllerBase
     {
-
+        ITransferDao transferDao = new TransferDao();
 
 
 
@@ -23,29 +25,35 @@ namespace TenmoServer.Controllers
 
         // TODO: GET List of past transactions from user
         [HttpGet("{id}")]
-        public IEnumerable<string> GetTransfers(int userId)
+        public IEnumerable<Transfer> GetTransfers(int userId)
         {
-            
-            return new string[] { "value1", "value2" };
+            return transferDao.GetTransfersForUser(userId);
         }
 
 
         // TODO: GET Transfer details by transaction ID
         // GET <TransferController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Transfer Get(int id)
         {
-            return "value";
+            return transferDao.GetTransferById(id);
         }
 
         // TODO: POST Transfer(int targetID, int senderID, decimal amountToSend)
         // POST <TransferController>
         [HttpPost("")]
-        public void Post([FromBody] int targetID, [FromBody] int senderID, [FromBody] decimal amountToSend)
+        public void Transfer([FromBody] int targetID, [FromBody] int senderID, [FromBody] decimal amountToSend)
         {
+            transferDao.SendTransfer(targetID, senderID, amountToSend);
         }
 
-        //TODO: GET check if user can afford a transfer
+        // TODO: GET check if user can afford a transfer
+        [HttpPost("")]
+        public void CheckIfViableTransfer([FromBody] int targetID, [FromBody] int senderID, [FromBody] decimal amountToSend)
+        {
+            transferDao.SendTransfer(targetID, senderID, amountToSend);
+        }
+
 
         // TODO: Put Decrease user balance by transfer amount
         // PUT <TransferController>/5
@@ -57,13 +65,5 @@ namespace TenmoServer.Controllers
 
         // TODO: GET status of transfer
         // TODO: PUT approve or deny transfer
-
-
-
-        // DELETE <TransferController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
