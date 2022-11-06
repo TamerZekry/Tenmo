35ddde23cabf48548ff7f5052bcfdec935c8ad41
Tamer Zekry <zekry.tamer@gmail.com>
2022-11-06 14:40:45 -0500

    Starting Unit testing project

---
+++ capstone/TestTenmo/DAO/BaseTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Transactions;
+
+namespace TestTenmo.DAO
+{
+    [TestClass]
+    public class BaseDaoTests
+    {
+        private const string DatabaseName = "tenmoTesting";
+        private const string AdminConnectionString = @"Server=.\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+        protected const string ConnectionString = @"Server=.\SQLEXPRESS;Database=" + DatabaseName + ";Trusted_Connection=True;";
+        private TransactionScope transaction;
+
+        [AssemblyInitialize]  
+        public static void BeforeAllTests(TestContext context)
+        {
+            string sql = File.ReadAllText("create_test_db.sql").Replace("test_db_name", DatabaseName);
+            using (SqlConnection conn = new SqlConnection(AdminConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+            
+            sql = File.ReadAllText("test_mock_data.sql");
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                 cmd.ExecuteNonQuery();
+            }
+        }
+
+        [AssemblyCleanup]  
+        public static void AfterAllTests()
+        {
+            string sql = File.ReadAllText("drop_test_db.sql").Replace("test_db_name", DatabaseName);
+            using (SqlConnection conn = new SqlConnection(AdminConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        [TestInitialize]  
+        public virtual void Setup()
+        {
+          transaction = new TransactionScope();
+        }
+
+        [TestCleanup]  
+        public void Cleanup()
+        {
+            transaction.Dispose();
+        }
+    }
+}
